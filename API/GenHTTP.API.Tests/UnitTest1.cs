62952da4a0789208ee2a279feb64e071ae6f3a08
Andreas Nägeli <andr.naegeli@gmail.com>
2019-03-30 18:28:19 +0100

    #10 - Basic unit testing structure

---
+++ API/GenHTTP.API.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace GenHTTP.API.Tests
+{
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+        
+        }
+
+    }
+
+}
---
+++ Modules/GenHTTP.Modules.Scriban.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace GenHTTP.Modules.Scriban.Tests
+{
+
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Test1()
+        {
+        
+        }
+
+    }
+
+}
