51c6bbf9e95697397f31f1555ed0c7b67c56ac00
Andreas Nägeli <andr.naegeli@gmail.com>
2019-11-06 17:43:47 +0100

    Change content type guessing to dictionary

--- Modules/GenHTTP.Modules.Core/CoreExtensions.cs
+++ Modules/GenHTTP.Modules.Core/CoreExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 using GenHTTP.Api.Modules;
@@ -10,71 +11,70 @@ namespace GenHTTP.Modules.Core
     public static class CoreExtensions
     {
 
+        #region Mappings
+
+        private static readonly Dictionary<string, ContentType> CONTENT_TYPES = new Dictionary<string, ContentType>() {
+            // CSS
+            { "css", ContentType.TextCss },
+            // HTML
+            { "html", ContentType.TextHtml }, 
+            { "htm", ContentType.TextHtml },
+            // Text files            
+            { "txt", ContentType.TextPlain },
+            { "conf", ContentType.TextPlain },
+            { "config", ContentType.TextPlain },
+            // Fonts 
+            { "eot", ContentType.FontEmbeddedOpenTypeFont }, 
+            { "ttf", ContentType.FontTrueTypeFont },
+            { "otf", ContentType.FontOpenTypeFont },
+            { "woff", ContentType.FontWoff },
+            { "woff2", ContentType.FontWoff2 },
+            // Scripts
+            { "js", ContentType.ApplicationJavaScript },
+            // Images
+            { "ico", ContentType.ImageIcon }, 
+            { "gif", ContentType.ImageGif }, 
+            { "jpeg", ContentType.ImageJpg },
+            { "jpg", ContentType.ImageJpg }, 
+            { "png", ContentType.ImagePng }, 
+            { "bmp", ContentType.ImageBmp },
+            { "tiff", ContentType.ImageTiff }, 
+            { "svg", ContentType.ImageScalableVectorGraphics }, 
+            { "svgz", ContentType.ImageScalableVectorGraphicsCompressed },
+            // Audio
+            { "ogg", ContentType.AudioOgg },
+            { "mp3", ContentType.AudioMpeg }, 
+            { "wav", ContentType.AudioWav },
+            // Video
+            { "mpg", ContentType.VideoMpeg },
+            { "mpeg", ContentType.VideoMpeg },
+            { "avi", ContentType.VideoMpeg },
+            // Documents
+            { "csv", ContentType.TextCsv }, 
+            { "rtf", ContentType.TextRichText }, 
+            { "docx", ContentType.ApplicationOfficeDocumentWordProcessing },
+            { "pptx", ContentType.ApplicationOfficeDocumentPresentation }, 
+            { "ppsx", ContentType.ApplicationOfficeDocumentSlideshow },
+            { "xslx", ContentType.ApplicationOfficeDocumentSheet },
+            // Object models
+            { "xml", ContentType.TextXml }
+        };
+
+        #endregion
+
+        #region Functionality
+
         public static ContentType? GuessContentType(this string fileName)
         {
             var extension = Path.GetExtension(fileName);
 
             if ((extension != null) && (extension.Length > 1))
             {
-                extension = extension.Substring(1);
+                extension = extension.Substring(1).ToLower();
 
-                switch (extension.ToLower())
+                if (CONTENT_TYPES.ContainsKey(extension))
                 {
-                    // CSS file
-                    case "css": return ContentType.TextCss;
-                    // HTML files
-                    case "html":
-                    case "htm": return ContentType.TextHtml;
-                    // Text files
-                    case "sql":
-                    case "txt":
-                    case "pl":
-                    case "cs":
-                    case "php":
-                    case "c":
-                    case "h":
-                    case "cpp":
-                    case "sh":
-                    case "bat":
-                    case "cmd":
-                    case "conf":
-                    case "ini":
-                    case "inf": return ContentType.TextPlain;
-                    // Fonts
-                    case "eot": return ContentType.FontEmbeddedOpenTypeFont;
-                    case "ttf": return ContentType.FontTrueTypeFont;
-                    case "otf": return ContentType.FontOpenTypeFont;
-                    case "woff": return ContentType.FontWoff;
-                    case "woff2": return ContentType.FontWoff2;
-                    // JavaScript
-                    case "js": return ContentType.ApplicationJavaScript;
-                    // Images
-                    case "ico": return ContentType.ImageIcon;
-                    case "gif": return ContentType.ImageGif;
-                    case "jpeg":
-                    case "jpg": return ContentType.ImageJpg;
-                    case "png": return ContentType.ImagePng;
-                    case "bmp": return ContentType.ImageBmp;
-                    case "tiff": return ContentType.ImageTiff;
-                    case "svg": return ContentType.ImageScalableVectorGraphics;
-                    case "svgz": return ContentType.ImageScalableVectorGraphicsCompressed;
-                    // Audio
-                    case "ogg": return ContentType.AudioOgg;
-                    case "mp3": return ContentType.AudioMpeg;
-                    case "wav": return ContentType.AudioWav;
-                    // Video files
-                    case "mpg":
-                    case "mpeg":
-                    case "avi": return ContentType.VideoMpeg;
-                    // Object models
-                    case "xml": return ContentType.TextXml;
-                    // Documents
-                    case "csv": return ContentType.TextCsv;
-                    case "rtf": return ContentType.TextRichText;
-                    case "docx": return ContentType.ApplicationOfficeDocumentWordProcessing;
-                    case "pptx": return ContentType.ApplicationOfficeDocumentPresentation;
-                    case "ppsx": return ContentType.ApplicationOfficeDocumentSlideshow;
-                    case "xlsx": return ContentType.ApplicationOfficeDocumentSheet;
+                    return CONTENT_TYPES[extension];
                 }
             }
 
@@ -128,6 +128,8 @@ namespace GenHTTP.Modules.Core
 
             return null;
         }
+
+        #endregion
 
     }
 
