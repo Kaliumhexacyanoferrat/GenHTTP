ab36e886c68721d53907a2b88bda017bb2b0dabe
Andreas Nägeli <andr.naegeli@gmail.com>
2020-03-31 15:43:10 +0200

    Add provider to generate XML sitemaps

---
+++ Modules/GenHTTP.Modules.Core/Sitemaps/SitemapContent.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+
+using GenHTTP.Api.Modules;
+using GenHTTP.Api.Protocol;
+
+namespace GenHTTP.Modules.Core.Sitemaps
+{
+
+    public class SitemapContent : IResponseContent
+    {
+        private static readonly XmlWriterSettings SETTINGS = new XmlWriterSettings()
+        {
+            Async = true,
+            CloseOutput = false
+        };
+
+        #region Get-/Setters
+
+        public ulong? Length => null;
+
+        private IEnumerable<ContentElement> Elements { get; }
+
+        #endregion
+
+        #region Initialization
+
+        public SitemapContent(IEnumerable<ContentElement> elements)
+        {
+            Elements = elements;
+        }
+
+        #endregion
+
+        #region Functionality
+
+        public async Task Write(Stream target, uint bufferSize)
+        {
+            using (var writer = XmlWriter.Create(target, SETTINGS))
+            {
+                await writer.WriteStartDocumentAsync();
+
+                {
+                    await writer.WriteStartElementAsync(null, "urlset", "http://www.sitemaps.org/schemas/sitemap/0.9");
+
+                    foreach (var element in Elements)
+                    {
+                        await writer.WriteStartElementAsync(null, "url", null);
+
+                        await writer.WriteElementStringAsync(null, "loc", null, element.Path);
+
+                        await writer.WriteEndElementAsync();
+                    }
+
+                    await writer.WriteEndElementAsync();
+                }
+
+                await writer.WriteEndDocumentAsync();
+            }
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Testing/GenHTTP.Testing.Acceptance/Providers/SitemapTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Xml.Serialization;
+
+using GenHTTP.Api.Routing;
+using GenHTTP.Modules.Core;
+using GenHTTP.Testing.Acceptance.Domain;
+
+using Xunit;
+
+namespace GenHTTP.Testing.Acceptance.Providers
+{
+
+    public class SitemapTests
+    {
+
+        #region Helping data structures
+
+        [XmlRoot("urlset", Namespace = "http://www.sitemaps.org/schemas/sitemap/0.9")]
+        public class UrlSet
+        {
+
+            [XmlElement("url")]
+            public List<Url>? Entries { get; set; }
+
+        }
+
+        public class Url
+        {
+
+            [XmlElement("loc")]
+            public string? Loc { get; set; }
+
+        }
+
+        #endregion
+
+        [Fact]
+        public void TestKnownSitemap()
+        {
+            using var runner = TestRunner.Run(GetContent());
+
+            var sitemap = GetSitemap(runner);
+
+            Assert.Contains("http://localhost/", sitemap);
+            Assert.Contains("http://localhost/other", sitemap);
+
+            Assert.Contains("http://localhost/children/", sitemap);
+            Assert.Contains("http://localhost/children/child-other", sitemap);
+
+            Assert.Equal(4, sitemap.Count);
+        }
+
+        [Fact]
+        public void TestNotFounds()
+        {
+            using var runner = TestRunner.Run(GetContent());
+
+            using var indexResponse = runner.GetResponse("/sitemaps/");
+            Assert.Equal(HttpStatusCode.NotFound, indexResponse.StatusCode);
+
+            using var fileResponse = runner.GetResponse("/sitemaps/some.txt");
+            Assert.Equal(HttpStatusCode.NotFound, fileResponse.StatusCode);
+        }
+
+        private HashSet<string> GetSitemap(TestRunner runner)
+        {
+            var serializer = new XmlSerializer(typeof(UrlSet));
+
+            using var response = runner.GetResponse("/sitemaps/sitemap.xml");
+
+            var sitemap = (UrlSet)serializer.Deserialize(response.GetResponseStream());
+
+            return sitemap.Entries.Select(u => u.Loc!.Replace(":" + runner.Port, string.Empty)).ToHashSet();
+        }
+
+        private IRouter GetContent()
+        {
+            var root = Layout.Create();
+
+            var children = Layout.Create();
+
+            children.Add("child-index", Page.From("Child Index Page", "Child Index"), true);
+            children.Add("child-other", Page.From("Child Other Page", "Child Other"), false);
+
+            var content = Layout.Create();
+
+            content.Add("index", Page.From("Index Page", "Index"), true);
+            content.Add("other", Page.From("Other Page", "Other"), false);
+
+            content.Add("children", children);
+
+            root.Add("sitemaps", Sitemap.From(content.Build()));
+
+            root.Default(content);
+
+            return root.Build();
+        }
+
+    }
+
+}
