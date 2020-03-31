7619672861ae11bd0413064a5fd701c0990a58c4
Andreas Nägeli <andr.naegeli@gmail.com>
2020-03-31 16:22:56 +0200

    Change sitemapt provider to respect children hierarchyy

--- Modules/GenHTTP.Modules.Core/Sitemaps/SitemapProvider.cs
+++ Modules/GenHTTP.Modules.Core/Sitemaps/SitemapProvider.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-
-using GenHTTP.Api.Modules;
+﻿using GenHTTP.Api.Modules;
 using GenHTTP.Api.Protocol;
 using GenHTTP.Api.Routing;
-
 using GenHTTP.Modules.Core.General;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GenHTTP.Modules.Core.Sitemaps
 {
@@ -33,12 +32,32 @@ namespace GenHTTP.Modules.Core.Sitemaps
         {
             var baseUri = $"{request.Client.Protocol.ToString().ToLower()}://{request.Host}/";
 
-            var elements = Content.GetContent(request, baseUri)
-                                  .Where(e => e.ContentType?.KnownType == Api.Protocol.ContentType.TextHtml);
+            var elements = new List<ContentElement>();
+
+            foreach (var element in Content.GetContent(request, baseUri))
+            {
+                Flatten(element, elements);
+            }
 
             return request.Respond()
                           .Content(new SitemapContent(elements))
                           .Type(Api.Protocol.ContentType.TextXml);
+        }
+
+        private void Flatten(ContentElement item, List<ContentElement> into)
+        {
+            if (item.ContentType?.KnownType == Api.Protocol.ContentType.TextHtml)
+            {
+                into.Add(item);
+            }
+
+            if (item.Children != null)
+            {
+                foreach (var child in item.Children)
+                {
+                    Flatten(child, into);
+                }
+            }
         }
 
         #endregion
