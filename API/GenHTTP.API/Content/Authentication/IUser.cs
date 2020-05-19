e637b7158a9070f7a19f3884dd74a86299166fb5
Andreas Nägeli <andr.naegeli@gmail.com>
2020-05-19 14:30:41 +0200

    Remove non required usings

--- API/GenHTTP.API/Content/Authentication/IUser.cs
+++ API/GenHTTP.API/Content/Authentication/IUser.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GenHTTP.Api.Content.Authentication
+﻿namespace GenHTTP.Api.Content.Authentication
 {
     
     public interface IUser
