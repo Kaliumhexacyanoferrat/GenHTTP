e160504d484b3a69f36405013976724b88549f24
Andreas Nägeli <andr.naegeli@gmail.com>
2020-08-31 16:24:34 +0200

    Add handler for Single Page Applications

---
+++ Modules/GenHTTP.Modules.Core/SinglePage/SinglePageBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+using GenHTTP.Api.Content;
+using GenHTTP.Api.Infrastructure;
+
+namespace GenHTTP.Modules.Core.SinglePage
+{
+
+    public class SinglePageBuilder : IHandlerBuilder<SinglePageBuilder>
+    {
+        private string? _Directory;
+
+        private readonly List<IConcernBuilder> _Concerns = new List<IConcernBuilder>();
+
+        #region Functionality
+
+        public SinglePageBuilder Add(IConcernBuilder concern)
+        {
+            _Concerns.Add(concern);
+            return this;
+        }
+
+        public SinglePageBuilder Directory(string directory)
+        {
+            _Directory = directory;
+            return this;
+        }
+
+        public IHandler Build(IHandler parent)
+        {
+            var directory = _Directory ?? throw new BuilderMissingPropertyException("directory");
+
+            return Concerns.Chain(parent, _Concerns, (p) => new SinglePageProvider(p, directory));
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Modules/GenHTTP.Modules.Core/SinglePageApplication.cs
@@ -0,0 +1,13 @@
+﻿using GenHTTP.Modules.Core.SinglePage;
+
+namespace GenHTTP.Modules.Core
+{
+
+    public static class SinglePageApplication
+    {
+
+        public static SinglePageBuilder From(string directory) => new SinglePageBuilder().Directory(directory);
+
+    }
+
+}
