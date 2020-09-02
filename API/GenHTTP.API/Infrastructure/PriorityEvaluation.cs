e6d043e98d54df9acfdcb6688d7beac69d210a7a
Andreas Nägeli <andr.naegeli@gmail.com>
2020-09-02 14:58:24 +0200

    Add basic load balancing mechanism

---
+++ API/GenHTTP.API/Infrastructure/PriorityEvaluation.cs
@@ -0,0 +1,11 @@
+﻿using GenHTTP.Api.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenHTTP.Api.Infrastructure
+{
+
+    public delegate Priority PriorityEvaluation(IRequest request);
+
+}
---
+++ Modules/GenHTTP.Modules.Core/LoadBalancing/LoadBalancerRedirectionHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using GenHTTP.Api.Content;
+using GenHTTP.Api.Protocol;
+
+namespace GenHTTP.Modules.Core.LoadBalancing
+{
+
+    public class LoadBalancerRedirectionHandler : IHandler
+    {
+
+        #region Get-/Setters
+
+        public IHandler Parent { get; }
+
+        private string Root { get; }
+
+        #endregion
+
+        #region Initialization
+
+        public LoadBalancerRedirectionHandler(IHandler parent, string root)
+        {
+            Parent = parent;
+
+            Root = root.EndsWith('/') ? root : $"{root}/";
+        }
+
+        #endregion
+
+        #region Functionality
+
+        public IResponse? Handle(IRequest request)
+        {
+            return Redirect.To(Root + request.Target.Current, true)
+                           .Build(this)
+                           .Handle(request);
+        }
+
+        public IEnumerable<ContentElement> GetContent(IRequest request) => Enumerable.Empty<ContentElement>();
+
+        #endregion
+
+    }
+
+}
