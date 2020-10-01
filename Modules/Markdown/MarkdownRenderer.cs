8304e062e3b7ea83c668e6dc5277cb3fb279aced
Andreas Nägeli <andr.naegeli@gmail.com>
2020-10-01 11:23:11 +0200

    Cosmetic changes to MarkdownRenderer

--- Modules/Markdown/MarkdownRenderer.cs
+++ Modules/Markdown/MarkdownRenderer.cs
@@ -1,24 +1,26 @@
 ﻿using GenHTTP.Api.Content;
 using GenHTTP.Api.Content.Templating;
+
 using GenHTTP.Modules.Basics;
+
 using Microsoft.DocAsCode.MarkdownLite;
 
 namespace GenHTTP.Modules.Markdown
 {
+
     public class MarkdownRenderer<T> : IRenderer<T> where T : class, IBaseModel
     {
-        private readonly IResourceProvider _FileProvider;
         private static readonly IMarkdownEngine _Engine = new GfmEngineBuilder(new Options()).CreateEngine(new HtmlRenderer());
+        
+        private readonly IResourceProvider _FileProvider;
 
         public MarkdownRenderer(IResourceProvider fileProvider)
         {
             _FileProvider = fileProvider;
         }
 
-        public string Render(T? model)
-        {
-            var markdown = _FileProvider.GetResourceAsString();
-            return _Engine.Markup(markdown);
-        }
+        public string Render(T? model) => _Engine.Markup(_FileProvider.GetResourceAsString());
+        
     }
+    
 }
