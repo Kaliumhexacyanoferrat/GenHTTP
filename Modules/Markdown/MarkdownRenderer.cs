122f3bb6d4b5e4bd1e467fcd2b2d328d81e61f48
Vitaly Derbin <vuderbin@gmail.com>
2020-10-01 12:25:20 +0300

    Add module to render pages from Markdown (#99)
    * add markdown rendering support
    
    * Cosmetic changes to MarkdownRenderer
    
    Co-authored-by: Andreas Nägeli <andr.naegeli@gmail.com>

---
+++ Modules/Markdown/MarkdownRenderer.cs
@@ -0,0 +1,26 @@
+﻿using GenHTTP.Api.Content;
+using GenHTTP.Api.Content.Templating;
+
+using GenHTTP.Modules.Basics;
+
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace GenHTTP.Modules.Markdown
+{
+
+    public class MarkdownRenderer<T> : IRenderer<T> where T : class, IBaseModel
+    {
+        private static readonly IMarkdownEngine _Engine = new GfmEngineBuilder(new Options()).CreateEngine(new HtmlRenderer());
+        
+        private readonly IResourceProvider _FileProvider;
+
+        public MarkdownRenderer(IResourceProvider fileProvider)
+        {
+            _FileProvider = fileProvider;
+        }
+
+        public string Render(T? model) => _Engine.Markup(_FileProvider.GetResourceAsString());
+        
+    }
+    
+}
