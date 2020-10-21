56177b4ea5c4ae28edd9b00d797e880e63620a33
Andreas Nägeli <andr.naegeli@gmail.com>
2020-10-21 15:28:05 +0200

    First changes for CORS

---
+++ Modules/Security/Cors/OriginPolicy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+using GenHTTP.Api.Protocol;
+
+namespace GenHTTP.Modules.Security.Cors
+{
+
+    public class OriginPolicy
+    {
+
+        #region Get-/Setters
+
+        /// <summary>
+        /// The HTTP methods the client is allowed to access (any, if not given).
+        /// </summary>
+        public List<FlexibleRequestMethod>? AllowedMethods { get; }
+
+        /// <summary>
+        /// The headers a client may send to the server (any, if not given).
+        /// </summary>
+        public List<string>? AllowedHeaders { get; }
+
+        /// <summary>
+        /// The headers that will be accessible by the client (any, if not given).
+        /// </summary>
+        public List<string>? ExposedHeaders { get; }
+
+        /// <summary>
+        /// Whether the client is allowed to read credentials from the request.
+        /// </summary>
+        public bool AllowCredentials { get; }
+
+        /// <summary>
+        /// The duration in seconds this policy is valid for.
+        /// </summary>
+        public uint MaxAge { get; }
+
+        #endregion
+
+        #region Initialization
+
+        public OriginPolicy(List<FlexibleRequestMethod>? allowedMethods, List<string>? allowedHeaders,
+                            List<string>? exposedHeaders, bool allowCredentials, uint maxAge)
+        {
+            AllowedMethods = allowedMethods;
+            AllowedHeaders = allowedHeaders;
+
+            ExposedHeaders = exposedHeaders;
+
+            AllowCredentials = allowCredentials;
+
+            MaxAge = maxAge;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Testing/Modules/Security/CorsTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+using Xunit;
+
+using GenHTTP.Api.Protocol;
+
+using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Layouting;
+using GenHTTP.Modules.Security;
+using GenHTTP.Modules.Security.Cors;
+
+namespace GenHTTP.Testing.Acceptance.Modules.Security
+{
+
+    public class CorsTests
+    {
+
+        [Fact]
+        public void TestPreflight()
+        {
+            using var runner = GetRunner(CorsPolicy.Permissive());
+
+            var request = runner.GetRequest("/t");
+
+            request.Method = "OPTIONS";
+
+            using var response = runner.GetResponse(request);
+
+            Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        [Fact]
+        public void TestPermissive()
+        {
+            using var runner = GetRunner(CorsPolicy.Permissive());
+
+            using var response = runner.GetResponse("/t");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.Equal("*", response.GetResponseHeader("Access-Control-Allow-Origin"));
+
+            Assert.Equal("*", response.GetResponseHeader("Access-Control-Allow-Methods"));
+            Assert.Equal("*", response.GetResponseHeader("Access-Control-Allow-Headers"));
+            Assert.Equal("*", response.GetResponseHeader("Access-Control-Expose-Headers"));
+
+            Assert.Equal("true", response.GetResponseHeader("Access-Control-Allow-Credentials"));
+
+            Assert.Equal("86400", response.GetResponseHeader("Access-Control-Max-Age"));
+        }
+
+        [Fact]
+        public void TestRestrictive()
+        {
+            using var runner = GetRunner(CorsPolicy.Restrictive());
+
+            using var response = runner.GetResponse("/t");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.DoesNotContain("Access-Control-Allow-Origin", response.Headers.AllKeys);
+
+            Assert.DoesNotContain("Access-Control-Allow-Methods", response.Headers.AllKeys);
+            Assert.DoesNotContain("Access-Control-Allow-Headers", response.Headers.AllKeys);
+            Assert.DoesNotContain("Access-Control-Expose-Headers", response.Headers.AllKeys);
+
+            Assert.DoesNotContain("Access-Control-Allow-Credentials", response.Headers.AllKeys);
+
+            Assert.DoesNotContain("Access-Control-Max-Age", response.Headers.AllKeys);
+        }
+
+        [Fact]
+        public void TestCustom()
+        {
+            var policy = CorsPolicy.Restrictive()
+                                   .Add("http://google.de", new List<FlexibleRequestMethod>() { new FlexibleRequestMethod(RequestMethod.GET) }, null, null, false);
+
+            using var runner = GetRunner(policy);
+
+            var request = runner.GetRequest("/t");
+
+            request.Headers.Add("Origin", "http://google.de");
+
+            using var response = runner.GetResponse(request);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            Assert.Equal("http://google.de", response.GetResponseHeader("Access-Control-Allow-Origin"));
+
+            Assert.Equal("GET", response.GetResponseHeader("Access-Control-Allow-Methods"));
+
+            Assert.Equal("Origin", response.GetResponseHeader("Vary"));
+        }
+
+        private TestRunner GetRunner(CorsPolicyBuilder policy)
+        {
+            var handler = Layout.Create()
+                                .Add("t", Content.From("Hello World"))
+                                .Add(policy);
+
+            return TestRunner.Run(handler);
+        }
+
+    }
+
+}
