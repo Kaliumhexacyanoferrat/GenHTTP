405072bb21c3f62356e77d0b2a93502321fa6c4e
Andreas Nägeli <andr.naegeli@gmail.com>
2020-11-02 18:16:36 +0100

    Refine tests

---
+++ Testing/Modules/IO/ResourceTreeTests.cs
@@ -0,0 +1,27 @@
+﻿using GenHTTP.Modules.IO;
+using System.Linq;
+using Xunit;
+
+namespace GenHTTP.Testing.Acceptance.Modules.IO
+{
+
+    public class ResourceTreeTests
+    {
+
+        [Fact]
+        public void TestAssembly()
+        {
+            var tree = ResourceTree.FromAssembly("Resources").Build();
+
+            Assert.True(tree.TryGetNode("Subdirectory", out var _));
+            
+            Assert.True(tree.TryGetResource("File.txt", out var _));
+
+            Assert.Single(tree.GetNodes());
+
+            Assert.Equal(4, tree.GetResources().Count());
+        }
+
+    }
+
+}
