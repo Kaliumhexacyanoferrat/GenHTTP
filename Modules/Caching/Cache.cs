c9bc2f2e71335487458ea7e4a11ef351a4041cc0
Andreas Nägeli <andr.naegeli@gmail.com>
2021-01-15 17:59:49 +0100

    Add documentation

--- Modules/Caching/Cache.cs
+++ Modules/Caching/Cache.cs
@@ -7,9 +7,16 @@ using GenHTTP.Modules.Caching.Memory;
 namespace GenHTTP.Modules.Caching
 {
 
+    /// <summary>
+    /// Creates caches with different kind of backends.
+    /// </summary>
     public static class Cache
     {
 
+        /// <summary>
+        /// A cache that will store all data in memory.
+        /// </summary>
+        /// <typeparam name="T">The type of the entries to be stored</typeparam>
         public static IMemoryCacheBuilder<T> Memory<T>()
         {
             if (typeof(Stream).IsAssignableFrom(typeof(T)))
@@ -22,12 +29,27 @@ namespace GenHTTP.Modules.Caching
             }
         }
 
+        /// <summary>
+        /// A cache that will store all data in the specified directory.
+        /// </summary>
+        /// <typeparam name="T">The type of the entries to be stored</typeparam>
+        /// <param name="directory">The directory to store the entries in</param>
         public static FileSystemCacheBuilder<T> FileSystem<T>(DirectoryInfo directory)
             => new FileSystemCacheBuilder<T>().Directory(directory);
 
+        /// <summary>
+        /// A cache that will store all data in the specified directory.
+        /// </summary>
+        /// <typeparam name="T">The type of the entries to be stored</typeparam>
+        /// <param name="directory">The directory to store the entries in</param>
         public static FileSystemCacheBuilder<T> FileSystem<T>(string directory)
             => FileSystem<T>(new DirectoryInfo(directory));
 
+        /// <summary>
+        /// A cache that will use files placed in the temp directory of the
+        /// operating system to store entries.
+        /// </summary>
+        /// <typeparam name="T">The type of the entries to be stored</typeparam>
         public static FileSystemCacheBuilder<T> TemporaryFiles<T>()
             => FileSystem<T>(new DirectoryInfo(Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString())));
 
