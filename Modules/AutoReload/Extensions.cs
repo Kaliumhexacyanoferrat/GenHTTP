8efdbb9437aa8fb4b2aad6939ea48513883018cf
Andreas Nägeli <andr.naegeli@gmail.com>
2021-07-09 10:02:45 +0200

    Stash

---
+++ Modules/AutoReload/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Websites.Sites;
+
+namespace GenHTTP.Modules.AutoReload
+{
+
+    public static class Extensions
+    {
+
+        public static WebsiteBuilder AutoReload(this WebsiteBuilder builder)
+        {
+            builder.AddScript("genhttp-autoreload.js", Resource.FromAssembly("AutoReload.js"), true);
+
+            return builder;
+        }
+
+    }
+
+}
---
+++ Modules/AutoReload/Script/AutoReload.js
@@ -0,0 +1,32 @@
+﻿var genhttp = {
+
+    interval: 2000,
+
+    getServerVersion: (callback) => {
+        var options = {
+            method: "HEAD"
+        };
+
+        fetch(document.location, options).then((response) => {
+            callback(response.headers.get("ETag"))
+        })
+    },
+
+    checkForModifications: (originalVersion) => {
+        this.getServerVersion((currentVersion) => {
+            if (originalVersion !== currentVersion) {
+                window.location.reload()
+            }
+            else {
+                this.getServerVersion((version) => setTimeout(checkForModifications, this.interval, version))
+            }
+        })
+    },
+
+    onLoad: () => {
+        this.getServerVersion((version) => setTimeout(checkForModifications, this.interval, version))
+    }
+
+};
+
+window.addEventListener('load', genhttp.onLoad);
