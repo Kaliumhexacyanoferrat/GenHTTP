1a03d5e9f4750e54e9d7aeea7a8474b9f7d84e21
Andreas Nägeli <andr.naegeli@gmail.com>
2022-12-13 12:48:29 +0100

    Add user injector and tests

--- Modules/Reflection/Injection.cs
+++ Modules/Reflection/Injection.cs
@@ -6,6 +6,8 @@ namespace GenHTTP.Modules.Reflection
     public static class Injection
     {
 
+        public static InjectionRegistryBuilder Empty() => new InjectionRegistryBuilder();
+
         public static InjectionRegistryBuilder Default()
         {
             return new InjectionRegistryBuilder().Add(new RequestInjector())
