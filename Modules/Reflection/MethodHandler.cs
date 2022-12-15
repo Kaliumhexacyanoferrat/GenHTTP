2275d7248fca1dd83c1b1337121f9958a97b5dd0
Andreas Nägeli <andr.naegeli@gmail.com>
2022-12-15 17:37:26 +0100

    Remove unnecessary using

--- Modules/Reflection/MethodHandler.cs
+++ Modules/Reflection/MethodHandler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.IO;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
 using System.Text.RegularExpressions;
