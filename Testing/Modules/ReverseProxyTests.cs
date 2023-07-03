ca7a3288cfbeadada95012e58bc9be33c1c37829
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2023-07-03 15:51:30 +0200

    Prepare for next iteration

--- Testing/Modules/ReverseProxyTests.cs
+++ Testing/Modules/ReverseProxyTests.cs
@@ -388,7 +388,7 @@ namespace GenHTTP.Testing.Acceptance.Providers
         public async Task TestBadGateway()
         {
             var proxy = Proxy.Create()
-                             .Upstream("http://127.0.0.2");
+                             .Upstream("http://127.1.0.2");
             
             using var runner = TestRunner.Run(proxy);
 
