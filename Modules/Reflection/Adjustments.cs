bdd087216a9535bf323c99c65648e4c74d36d97a
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-01-04 13:33:52 +0100

    Polishment

--- Modules/Reflection/Adjustments.cs
+++ Modules/Reflection/Adjustments.cs
@@ -8,12 +8,16 @@ namespace GenHTTP.Modules.Reflection
     internal static class Adjustments
     {
 
+        /// <summary>
+        /// Allows to chain the execution of the given adjustments into
+        /// the given response builder.
+        /// </summary>
+        /// <param name="builder">The response builder to be adjusted</param>
+        /// <param name="adjustments">The adjustments to be executed (if any)</param>
+        /// <returns>The response builder to be chained</returns>
         internal static IResponseBuilder Adjust(this IResponseBuilder builder, Action<IResponseBuilder>? adjustments)
         {
-            if (adjustments != null)
-            {
-                adjustments(builder);
-            }
+            adjustments?.Invoke(builder);
 
             return builder;
         }
