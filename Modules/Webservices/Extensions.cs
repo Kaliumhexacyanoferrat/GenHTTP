4f7a275e4d035f7682de092469d426ff40b86893
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-02-21 14:44:29 +0100

    Allow to pass serialization and injection factories for web services

--- Modules/Webservices/Extensions.cs
+++ Modules/Webservices/Extensions.cs
@@ -1,6 +1,11 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
+using GenHTTP.Api.Infrastructure;
+
+using GenHTTP.Modules.Conversion.Providers;
 using GenHTTP.Modules.Layouting.Provider;
+using GenHTTP.Modules.Reflection.Injectors;
+using GenHTTP.Modules.Webservices.Provider;
 
 namespace GenHTTP.Modules.Webservices
 {
@@ -17,9 +22,11 @@ namespace GenHTTP.Modules.Webservices
         /// </summary>
         /// <typeparam name="T">The type of the resource to be added</typeparam>
         /// <param name="path">The path the resource should be available at</param>
-        public static LayoutBuilder AddService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(this LayoutBuilder layout, string path) where T : new()
+        /// <param name="injectors">Optionally the injectors to be used by this service</param>
+        /// <param name="formats">Optionally the formats to be used by this service</param>
+        public static LayoutBuilder AddService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(this LayoutBuilder layout, string path, IBuilder<InjectionRegistry>? injectors = null, IBuilder<SerializationRegistry>? formats = null) where T : new()
         {
-            return layout.Add(path, ServiceResource.From<T>());
+            return layout.Add(path, ServiceResource.From<T>().Configured(injectors, formats));
         }
 
         /// <summary>
@@ -28,9 +35,26 @@ namespace GenHTTP.Modules.Webservices
         /// </summary>
         /// <param name="path">The path the resource should be available at</param>
         /// <param name="instance">The webservice resource instance</param>
-        public static LayoutBuilder AddService(this LayoutBuilder layout, string path, object instance)
+        /// <param name="injectors">Optionally the injectors to be used by this service</param>
+        /// <param name="formats">Optionally the formats to be used by this service</param>
+        public static LayoutBuilder AddService(this LayoutBuilder layout, string path, object instance, IBuilder<InjectionRegistry>? injectors = null, IBuilder<SerializationRegistry>? formats = null)
         {
-            return layout.Add(path, ServiceResource.From(instance));
+            return layout.Add(path, ServiceResource.From(instance).Configured(injectors, formats));
+        }
+
+        private static ServiceResourceBuilder Configured(this ServiceResourceBuilder builder, IBuilder<InjectionRegistry>? injectors = null, IBuilder<SerializationRegistry>? formats = null)
+        {
+            if (injectors != null)
+            {
+                builder.Injectors(injectors);
+            }
+
+            if (formats != null)
+            {
+                builder.Formats(formats);
+            }
+
+            return builder;
         }
 
     }
