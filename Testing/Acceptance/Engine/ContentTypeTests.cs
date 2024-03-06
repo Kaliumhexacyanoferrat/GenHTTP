8eb268d922d627372788628adbc81c8072ff6c4f
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-03-06 17:09:41 +0100

    Add content types for typical video formats

---
+++ Testing/Acceptance/Engine/ContentTypeTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using GenHTTP.Api.Protocol;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenHTTP.Testing.Acceptance.Engine
+{
+
+    [TestClass]
+    public sealed class ContentTypeTests
+    {
+
+        [TestMethod]
+        public void MapContentTypeTests()
+        {
+            foreach (ContentType contentType in Enum.GetValues(typeof(ContentType)))
+            {
+                var mapped = new FlexibleContentType(contentType);
+
+                Assert.AreEqual(mapped.KnownType, contentType);
+            }
+        }
+
+    }
+
+}
