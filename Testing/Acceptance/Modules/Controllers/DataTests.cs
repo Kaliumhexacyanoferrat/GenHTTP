8caf3ebd9bb073667bd271b0094f91d9584471f1
Kaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-03-11 17:05:47 +0100

    Add tests

--- Testing/Acceptance/Modules/Controllers/DataTests.cs
+++ Testing/Acceptance/Modules/Controllers/DataTests.cs
@@ -8,6 +8,7 @@ using GenHTTP.Api.Protocol;
 using GenHTTP.Modules.Controllers;
 using GenHTTP.Modules.Conversion;
 using GenHTTP.Modules.Layouting;
+using GenHTTP.Modules.Reflection;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -53,6 +54,25 @@ namespace GenHTTP.Testing.Acceptance.Modules.Controllers
             Assert.AreEqual("2024-03-11", await response.GetContentAsync());
         }
 
+        [TestMethod]
+        public async Task TestInvalidDateOnly()
+        {
+            using var host = GetHost();
+
+            var request = host.GetRequest("/t/date/", HttpMethod.Post);
+
+            var data = new Dictionary<string, string>()
+            {
+                { "date", "ABC" }
+            };
+
+            request.Content = new FormUrlEncodedContent(data);
+
+            using var response = await host.GetResponseAsync(request);
+
+            await response.AssertStatusAsync(HttpStatusCode.BadRequest);
+        }
+
         #endregion
 
         #region Helpers
@@ -60,7 +80,7 @@ namespace GenHTTP.Testing.Acceptance.Modules.Controllers
         private static TestHost GetHost()
         {
             var app = Layout.Create()
-                            .AddController<TestController>("t", formats: Serialization.Default());
+                            .AddController<TestController>("t", formats: Serialization.Default(), injectors: Injection.Default());
 
             return TestHost.Run(app);
         }
