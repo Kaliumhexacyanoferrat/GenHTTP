576b6f547cadb37200e3ebfe174046192daa1225
Andreas Nägeli <andr.naegeli@gmail.com>
2024-07-01 15:34:00 +0200

    Add structured error mapper (#507)

---
+++ Modules/ErrorHandling/StructuredErrorMapper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using GenHTTP.Api.Content;
+using GenHTTP.Api.Protocol;
+
+using GenHTTP.Modules.Conversion;
+using GenHTTP.Modules.Conversion.Providers;
+using GenHTTP.Modules.Conversion.Providers.Json;
+
+namespace GenHTTP.Modules.ErrorHandling
+{
+
+    public sealed class StructuredErrorMapper : IErrorMapper<Exception>
+    {
+
+        #region Supporting data structures
+
+        public record class ErrorModel(ResponseStatus Status, string Message, string? StackTrace = null);
+
+        #endregion
+
+        #region Get-/Setters
+
+        public SerializationRegistry Registry { get; }
+
+        #endregion
+
+        #region Initialization
+
+        public StructuredErrorMapper(SerializationRegistry? registry)
+        {
+            Registry = registry ?? Serialization.Default().Build();
+        }
+
+        #endregion
+
+        #region Functionality
+
+        public async ValueTask<IResponse?> Map(IRequest request, IHandler handler, Exception error)
+        {
+            string? stackTrace = null;
+
+            if (request.Server.Development)
+            {
+                stackTrace = error.StackTrace;
+            }
+
+            if (error is ProviderException e)
+            {
+                var model = new ErrorModel(e.Status, error.Message, stackTrace);
+
+                return await RenderAsync(request, model);
+            }
+            else
+            {
+                var model = new ErrorModel(ResponseStatus.InternalServerError, error.Message, stackTrace);
+
+                return await RenderAsync(request, model);
+            }
+        }
+
+        public async ValueTask<IResponse?> GetNotFound(IRequest request, IHandler handler)
+        {
+            var model = new ErrorModel(ResponseStatus.NotFound, "The requested resource does not exist on this server");
+
+            return await RenderAsync(request, model);
+        }
+
+        private async ValueTask<IResponse> RenderAsync(IRequest request, ErrorModel model)
+        {
+            var format = Registry.GetSerialization(request) ?? new JsonFormat();
+
+            var response = await format.SerializeAsync(request, model);
+
+            response.Status(model.Status);
+
+            return response.Build();
+        }
+
+        #endregion
+
+    }
+
+}
