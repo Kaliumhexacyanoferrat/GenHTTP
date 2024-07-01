7467841e19da2a5d3c4fe065f948559bcf093dae
AKaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-07-01 08:50:51 +0200

    Add test extensions to easily deserialize responses

---
+++ Testing/Acceptance/Testing/ContentTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using GenHTTP.Api.Protocol;
+using GenHTTP.Modules.Functional;
+using GenHTTP.Modules.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenHTTP.Testing.Acceptance.Testing
+{
+
+    [TestClass]
+    public sealed class ContentTests
+    {
+
+        public record class MyType(int ID);
+
+        [TestMethod]
+        public async Task TestDeserialization()
+        {
+            var expectation = new MyType(42);
+
+            var handler = Inline.Create()
+                                .Get(() => expectation);
+
+            using var host = TestHost.Run(handler);
+
+            using var response = await host.GetResponseAsync();
+
+            Assert.AreEqual(expectation, await response.GetContentAsync<MyType>());
+        }
+
+        [TestMethod]
+        public async Task TestNull()
+        {
+            var handler = Inline.Create()
+                                .Get(() => (MyType?)null);
+
+            using var host = TestHost.Run(handler);
+
+            using var response = await host.GetResponseAsync();
+
+            Assert.IsNull(await response.GetOptionalContentAsync<MyType>());
+        }
+
+        [TestMethod]
+        public async Task TestUnsupported()
+        {
+            var handler = Inline.Create()
+                                .Get(() => new Result<string>("Nah").Type(FlexibleContentType.Get("text/html")));
+
+            using var host = TestHost.Run(handler);
+
+            using var response = await host.GetResponseAsync();
+
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () =>
+            {
+                await response.GetOptionalContentAsync<MyType>();
+            });
+        }
+
+    }
+
+}
---
+++ Testing/Testing/HeaderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Net.Http;
+
+namespace GenHTTP.Testing
+{
+    
+    public static class HeaderExtensions
+    {
+
+        public static string? GetHeader(this HttpResponseMessage response, string key)
+        {
+            if (response.Headers.TryGetValues(key, out var values))
+            {
+                return values.FirstOrDefault();
+            }
+
+            return null;
+        }
+
+        public static string? GetContentHeader(this HttpResponseMessage response, string key)
+        {
+            if (response.Content.Headers.TryGetValues(key, out var values))
+            {
+                return values.FirstOrDefault();
+            }
+
+            return null;
+        }
+
+    }
+
+}
