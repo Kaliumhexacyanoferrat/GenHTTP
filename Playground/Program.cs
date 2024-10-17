099d73241f8cdc6779d80625fd10e82d8c5a8b11
Andreas Nägeli <andr.naegeli@gmail.com>
2024-10-17 17:46:51 +0200

    Restore original playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,52 +1,12 @@
-﻿using GenHTTP.Api.Content;
-using GenHTTP.Api.Protocol;
-using GenHTTP.Engine;
-using GenHTTP.Modules.Basics;
-using GenHTTP.Modules.Controllers;
-using GenHTTP.Modules.Functional;
-using GenHTTP.Modules.OpenApi;
+﻿using GenHTTP.Engine;
+using GenHTTP.Modules.IO;
 using GenHTTP.Modules.Practices;
-using GenHTTP.Modules.Webservices;
 
-// todo bug: Inline.Get("/") => wirft fehler!
-
-var description = ApiDescription.Create()
-                                .Title("My API")
-                                .Version("1.0.0");
-
-var inline = Inline.Create()
-                   .Put("file", (Stream stream) => true)
-                   .Add(description);
+var app = Content.From(Resource.FromString("Hello World"));
 
 Host.Create()
-    .Handler(inline)
+    .Handler(app)
     .Defaults()
     .Development()
     .Console()
     .Run();
-
-public record User(int ID, string Name);
-
-public class UserService
-{
-
-    [ResourceMethod]
-    public Stream Avatar(DateTime cannot, short s, byte b, bool b2) => new MemoryStream();
-}
-
-public class DeviceController
-{
-
-    // [ControllerAction(RequestMethod.Post)]
-    public void Register(int id)
-    {
-
-    }
-
-    [ControllerAction(RequestMethod.Get)]
-    public IHandlerBuilder Wildcard() => Redirect.To("https://google.de");
-
-    [Obsolete]
-    [ControllerAction(RequestMethod.Get)]
-    public ValueTask<User?> GetUserAsync() => new();
-}
