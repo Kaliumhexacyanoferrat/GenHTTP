623203eb657ad51660dce94f8d2d9601b5a8391c
AKaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-10-20 01:41:54 +0200

    Add documentation

--- Modules/Websockets/Handler/WebsocketHandlerBuilder.cs
+++ Modules/Websockets/Handler/WebsocketHandlerBuilder.cs
@@ -24,48 +24,80 @@ public class WebsocketHandlerBuilder : IHandlerBuilder<WebsocketHandlerBuilder>
         return this;
     }
 
+    /// <summary>
+    /// Specifies a protocol supported by this websocket server.
+    /// </summary>
+    /// <param name="supportedProtocol">The name of the supported protocol</param>
     public WebsocketHandlerBuilder Protocol(string supportedProtocol)
     {
         _SupportedProtocols.Add(supportedProtocol);
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if a new websocket client connected.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnOpen(Action<IWebsocketConnection> handler)
     {
         _OnOpen = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if a websocket client disconnects.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnClose(Action<IWebsocketConnection> handler)
     {
         _OnClose = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if a string message has been received from the client.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnMessage(Action<IWebsocketConnection, string> handler)
     {
         _OnMessage = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if a binary message has been received from the client.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnBinary(Action<IWebsocketConnection, byte[]> handler)
     {
         _OnBinary = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if the client sends a ping request.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnPing(Action<IWebsocketConnection, byte[]> handler)
     {
         _OnPing = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if the client sends a pong request.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnPong(Action<IWebsocketConnection, byte[]> handler)
     {
         _OnPong = handler;
         return this;
     }
 
+    /// <summary>
+    /// Will be executed if there is some client connection error.
+    /// </summary>
+    /// <param name="handler">The method to be executed</param>
     public WebsocketHandlerBuilder OnError(Action<IWebsocketConnection, Exception> handler)
     {
         _OnError = handler;
--- Modules/Websockets/Websocket.cs
+++ Modules/Websockets/Websocket.cs
@@ -5,6 +5,10 @@ namespace GenHTTP.Modules.Websockets;
 public static class Websocket
 {
 
+    /// <summary>
+    /// Creates a new builder to configure a websocket handler
+    /// that will process incoming websocket requests.
+    /// </summary>
     public static WebsocketHandlerBuilder Create() => new();
 
 }
