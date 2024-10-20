c038f69b8c3e76b620540ca226867f4140506fab
AKaliumhexacyanoferrat <andr.naegeli@gmail.com>
2024-10-20 02:00:14 +0200

    Restore original playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,47 +1,12 @@
 ﻿using GenHTTP.Engine;
+using GenHTTP.Modules.IO;
 using GenHTTP.Modules.Practices;
-using GenHTTP.Modules.Websockets;
 
-var allSockets = new List<IWebsocketConnection>();
+var app = Content.From(Resource.FromString("Hello World"));
 
-var websocket = Websocket.Create()
-                         .OnOpen((socket) =>
-                         {
-                             Console.WriteLine("Open!");
-                             allSockets.Add(socket);
-                         })
-                         .OnClose((socket) =>
-                         {
-                             Console.WriteLine("Close!");
-                             allSockets.Remove(socket);
-                         })
-                         .OnMessage((socket, message) =>
-                         {
-                             Console.WriteLine(message);
-                             allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
-                         });
-
-var host = Host.Create()
-    .Handler(websocket)
+Host.Create()
+    .Handler(app)
     .Defaults()
     .Development()
-    .Console();
-
-host.Start();
-
-var input = Console.ReadLine();
-
-while (input != "exit")
-{
-    if (input != null)
-    {
-        foreach (var socket in allSockets.ToList())
-        {
-            await socket.Send(input);
-        }
-    }
-
-    input = Console.ReadLine();
-}
-
-host.Stop();
+    .Console()
+    .Run();
