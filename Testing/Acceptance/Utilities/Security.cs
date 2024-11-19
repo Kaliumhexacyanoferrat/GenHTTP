8c7219c68d194405c340bc46bc0503e40b6805b3
Andreas Nägeli <andr.naegeli@gmail.com>
2024-11-19 18:19:43 +0100

    Test happy path (:

---
+++ Testing/Acceptance/Utilities/Security.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using GenHTTP.Modules.IO;
+
+namespace GenHTTP.Testing.Acceptance.Utilities;
+
+public static class Security
+{
+
+    public static async ValueTask<X509Certificate2> GetCertificateAsync()
+    {
+        await using var stream = await Resource.FromAssembly("Certificate.pfx").Build().GetContentAsync();
+
+        using var mem = new MemoryStream();
+
+        await stream.CopyToAsync(mem);
+#if NET8_0
+        return new X509Certificate2(mem.ToArray());
+#else
+        return X509CertificateLoader.LoadPkcs12(mem.ToArray(), null);
+#endif
+    }
+
+}
