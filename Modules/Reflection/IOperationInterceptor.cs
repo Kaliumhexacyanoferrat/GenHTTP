1ebcae5500d590b740ffe25cfa90e236530cd917
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-05 22:06:58 +0100

    Add documentation

--- Modules/Reflection/IOperationInterceptor.cs
+++ Modules/Reflection/IOperationInterceptor.cs
@@ -4,13 +4,36 @@ using GenHTTP.Modules.Reflection.Operations;
 
 namespace GenHTTP.Modules.Reflection;
 
+/// <summary>
+/// A result returned by an interceptor.
+/// </summary>
+/// <param name="payload">The payload of the response</param>
 public sealed class InterceptionResult(object? payload = null) : Result<object?>(payload);
 
+/// <summary>
+/// A piece of logic to be executed before the
+/// actual method invocation. Triggered by methods
+/// annotated with the <see cref="InterceptWithAttribute{T}" />
+/// attribute.
+/// </summary>
 public interface IOperationInterceptor
 {
 
+    /// <summary>
+    /// Invoked after the instance has been created to configure
+    /// the interceptor with the originally used attribute. Allows
+    /// the interceptor to read configuration data as needed.
+    /// </summary>
+    /// <param name="attribute">The original attribute instance on the method definition</param>
     void Configure(object attribute);
 
+    /// <summary>
+    /// Invoked on every operation call by the client.
+    /// </summary>
+    /// <param name="request">The request which caused this invocation</param>
+    /// <param name="operation">The currently executed operation</param>
+    /// <param name="arguments">The operation arguments as derived by the framework</param>
+    /// <returns>If a result is returned, it will be converted into a response and the method is not invoked</returns>
     ValueTask<InterceptionResult?> InterceptAsync(IRequest request, Operation operation, IReadOnlyDictionary<string, object?> arguments);
 
 }
--- Modules/Reflection/InterceptWithAttribute.cs
+++ Modules/Reflection/InterceptWithAttribute.cs
@@ -1,5 +1,14 @@
 ﻿namespace GenHTTP.Modules.Reflection;
 
+/// <summary>
+/// When annotated on a service method, the method handler
+/// will create an instance of T and invoke it before
+/// the actual method invocation.
+/// </summary>
+/// <typeparam name="T">The type of interceptor to be used</typeparam>
+/// <remarks>
+/// Allows to implement concerns on operation level such as authorization.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Method)]
 public class InterceptWithAttribute<T> : Attribute where T : IOperationInterceptor, new()
 {
