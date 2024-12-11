8dbdc25e54e4b11b48d6e5ed8de4d7678740e83a
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-11 13:49:19 +0100

    Add small delay to test file utilities to prevent race condition with Kestrel (#586)

--- Testing/Acceptance/Utilities/FileUtil.cs
+++ Testing/Acceptance/Utilities/FileUtil.cs
@@ -15,9 +15,16 @@ public static class FileUtil
 
     public static async ValueTask WriteTextAsync(string path, string content)
     {
+        // Kestrel flushes its output buffers as soon as the content length sent in
+        // the headers is reached, causing the test execution to continue and introducing
+        // a race condition where the test writes again to the same file that is not
+        // yet disposed by the server. As there is no way around this, we will be hacky here.
+        await Task.Delay(100);
+
         await using var file = File.Create(path, 4096, FileOptions.WriteThrough);
 
         await file.WriteAsync(Encoding.UTF8.GetBytes(content));
         await file.FlushAsync();
     }
+
 }
