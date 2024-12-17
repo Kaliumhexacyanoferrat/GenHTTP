3db687041d43dca3e3206be4be04cbd6253fbbfe
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-17 19:12:00 +0100

    Improve code coverage

---
+++ Testing/Acceptance/Modules/Functional/IntegrationTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using GenHTTP.Modules.Conversion;
+using GenHTTP.Modules.Conversion.Formatters;
+using GenHTTP.Modules.Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenHTTP.Testing.Acceptance.Modules.Functional;
+
+[TestClass]
+public class IntegrationTest
+{
+
+    [TestMethod]
+    [MultiEngineTest]
+    public async Task TestFormatters(TestEngine engine)
+    {
+        var formatting = Formatting.Empty()
+                                   .Add<BoolFormatter>();
+
+        var api = Inline.Create()
+                        .Any("get-bool", (bool value) => value)
+                        .Formatters(formatting);
+
+        await using var host = await TestHost.RunAsync(api, engine: engine);
+
+        using var response = await host.GetResponseAsync("/get-bool?value=1");
+
+        await response.AssertStatusAsync(HttpStatusCode.OK);
+
+        Assert.AreEqual("1", await response.GetContentAsync());
+    }
+
+}
---
+++ Testing/Acceptance/Modules/Functional/MethodTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using GenHTTP.Modules.Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenHTTP.Testing.Acceptance.Modules.Functional;
+
+[TestClass]
+public class MethodTest
+{
+
+    [TestMethod]
+    [MultiEngineTest]
+    public async Task TestAnyMethod(TestEngine engine)
+    {
+        var app = Inline.Create()
+                        .Any((List<int> data) => data.Count);
+
+        await using var host = await TestHost.RunAsync(app, engine: engine);
+
+        foreach (var method in new[]
+                 {
+                     HttpMethod.Post, HttpMethod.Put
+                 })
+        {
+            var request = host.GetRequest(method: method);
+
+            request.Content = new StringContent("[ 1, 2 ]");
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            using var response = await host.GetResponseAsync(request);
+
+            await response.AssertStatusAsync(HttpStatusCode.OK);
+
+            Assert.AreEqual("2", await response.GetContentAsync());
+        }
+    }
+
+    [TestMethod]
+    [MultiEngineTest]
+    public async Task TestDelete(TestEngine engine)
+    {
+        var app = Inline.Create()
+                        .Delete(() => { });
+
+        await using var host = await TestHost.RunAsync(app, engine: engine);
+
+        var request = host.GetRequest(method: HttpMethod.Delete);
+
+        using var response = await host.GetResponseAsync(request);
+
+        await response.AssertStatusAsync(HttpStatusCode.NoContent);
+    }
+
+    [TestMethod]
+    [MultiEngineTest]
+    public async Task TestHead(TestEngine engine)
+    {
+        var app = Inline.Create()
+                        .Head(() => "42");
+
+        await using var host = await TestHost.RunAsync(app, engine: engine);
+
+        var request = host.GetRequest(method: HttpMethod.Head);
+
+        using var response = await host.GetResponseAsync(request);
+
+        await response.AssertStatusAsync(HttpStatusCode.OK);
+
+        Assert.AreEqual("2", response.GetContentHeader("Content-Length"));
+    }
+
+}
