336195f7f611dc73b09a1aac72bdf197a46af7af
Andreas Nägeli <andr.naegeli@gmail.com>
2024-12-18 17:44:15 +0100

    Improve code coverage (#598)

---
+++ Testing/Acceptance/Engine/RoutingTests.cs
@@ -0,0 +1,37 @@
+﻿using GenHTTP.Api.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenHTTP.Testing.Acceptance.Engine;
+
+[TestClass]
+public class RoutingTests
+{
+
+    [TestMethod]
+    public void TestComparers()
+    {
+        var path = new WebPathPart("%C3%A4%2F");
+
+        Assert.IsTrue(path == "ä/");
+        Assert.IsTrue(path == "%C3%A4%2F");
+
+        Assert.IsTrue(path != "ö/");
+    }
+
+    [TestMethod]
+    public void TestEquality()
+    {
+        var one = new WebPathPart("%C3%A4%2F");
+        var two = new WebPathPart("%C3%A4%2F");
+
+        Assert.IsTrue(one.Equals(two));
+        Assert.IsTrue(two.Equals(one));
+
+        var three = new WebPathPart("ä/");
+        
+        Assert.IsFalse(one.Equals(three));
+        Assert.IsFalse(one.Equals(new List<int>()));
+        Assert.IsFalse(one.Equals(null));
+    }
+
+}
