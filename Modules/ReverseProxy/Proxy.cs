21cbf6bb6030368abc0b1e75ecf7871673e0b783
Andreas Nägeli <andr.naegeli@gmail.com>
2025-01-21 14:19:07 +0100

    Allow to adjust the handler and client used by the reverse proxy

--- Modules/ReverseProxy/Proxy.cs
+++ Modules/ReverseProxy/Proxy.cs
@@ -5,5 +5,11 @@ namespace GenHTTP.Modules.ReverseProxy;
 public static class Proxy
 {
 
+    /// <summary>
+    /// Creates a handler that will route incoming requests to
+    /// the specified upstream server.
+    /// </summary>
+    /// <returns>The newly created handler</returns>
     public static ReverseProxyBuilder Create() => new();
+
 }
