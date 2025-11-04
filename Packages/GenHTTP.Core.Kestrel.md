1628db8f1c45a5cea119d883f8d1a849ca4e6b37
Andreas Nägeli <andr.naegeli@gmail.com>
2025-11-04 19:09:32 +0100

    Add readmes for meta packages

---
+++ Packages/GenHTTP.Core.Kestrel.md
@@ -0,0 +1,22 @@
+﻿# GenHTTP.Core.Kestrel
+
+This package provides the basic dependencies
+required to run a GenHTTP application using
+Kestrel as a server engine.
+
+Please refer to [the documentation](https://genhttp.org/documentation/)
+for tutorials, template projects and other examples.
+
+```csharp
+using GenHTTP.Engine.Kestrel;
+
+using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Practices;
+
+var content = Content.From(Resource.FromString("Hello World!"));
+
+await Host.Create()
+          .Handler(content)
+          .Defaults()
+          .RunAsync(); // or StartAsync() for non-blocking
+```
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,50 +1,10 @@
-﻿using GenHTTP.Api.Protocol;
-
-using GenHTTP.Engine.Internal;
-
+﻿using GenHTTP.Engine.Internal;
 using GenHTTP.Modules.IO;
-using GenHTTP.Modules.IO.Streaming;
-using GenHTTP.Modules.Layouting;
 using GenHTTP.Modules.Practices;
-using GenHTTP.Modules.Webservices;
 
-var app = Layout.Create()
-                .AddService<ContentExamples>("content");
+var content = Content.From(Resource.FromString("Hello World!"));
 
 await Host.Create()
-          .Handler(app)
+          .Handler(content)
           .Defaults()
-          .Console()
-          .RunAsync();
-
-class ContentExamples
-{
-
-    [ResourceMethod("get-string")]
-    public IResponseBuilder GetString(IRequest request)
-    {
-        return request.Respond()
-                      .Content("This is a string");
-    }
-
-    [ResourceMethod("get-resource")]
-    public IResponseBuilder GetResource(IRequest request)
-    {
-        var resource = Resource.FromString("This is a string") // or from any other source
-                               .Build();
-
-        return request.Respond()
-                      .Content(resource);
-    }
-
-    [ResourceMethod("get-stream")]
-    public IResponseBuilder GetStream(IRequest request)
-    {
-        var stream = new MemoryStream("This is a string"u8.ToArray());
-
-        return request.Respond()
-                      .Content(stream, (ulong)stream.Length, stream.CalculateChecksumAsync)
-                      .Type(FlexibleContentType.Get(ContentType.TextPlain));
-    }
-
-}
+          .RunAsync(); // or StartAsync() for non-blocking
