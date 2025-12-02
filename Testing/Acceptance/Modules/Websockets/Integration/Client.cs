249dca776a92a8e248280ac34464c151b8a6304d
Diogo Martins <diogoalves@ua.pt>
2025-12-02 10:40:47 +0000

    Re enable reactive integration, try not use continue

--- Testing/Acceptance/Modules/Websockets/Integration/Client.cs
+++ Testing/Acceptance/Modules/Websockets/Integration/Client.cs
@@ -6,19 +6,21 @@ namespace GenHTTP.Testing.Acceptance.Modules.Websockets.Integration;
 
 public static class Client
 {
-
     public static async ValueTask Execute(int port)
     {
+        var cts = new CancellationTokenSource(2000);
+        var token = cts.Token;
+
         var client = new ClientWebSocket();
 
-        await client.ConnectAsync(new Uri($"ws://localhost:{port}"), TimeoutToken());
+        await client.ConnectAsync(new Uri($"ws://localhost:{port}"), token);
 
         // Sending a Text message
         var bytes = "Hello, World!"u8.ToArray();
-        await client.SendAsync(bytes, WebSocketMessageType.Text, true, TimeoutToken());
+        await client.SendAsync(bytes, WebSocketMessageType.Text, true, token);
 
         var responseBuffer = new byte[bytes.Length];
-        var response = await client.ReceiveAsync(new ArraySegment<byte>(responseBuffer), TimeoutToken());
+        var response = await client.ReceiveAsync(new ArraySegment<byte>(responseBuffer), token);
 
         Assert.AreEqual(WebSocketMessageType.Text, response.MessageType);
         Assert.AreEqual("Hello, World!", Encoding.UTF8.GetString(responseBuffer));
@@ -30,8 +32,8 @@ public static class Client
         var firstFrame = "This is the first segment"u8.ToArray();
         var firstResponseBuffer = new byte[firstFrame.Length];
 
-        await client.SendAsync(firstFrame, WebSocketMessageType.Text, false, TimeoutToken());
-        response = await client.ReceiveAsync(new ArraySegment<byte>(firstResponseBuffer), TimeoutToken());
+        await client.SendAsync(firstFrame, WebSocketMessageType.Text, true, token);
+        response = await client.ReceiveAsync(new ArraySegment<byte>(firstResponseBuffer), token);
 
         Assert.AreEqual(WebSocketMessageType.Text, response.MessageType);
         Assert.AreEqual("This is the first segment", Encoding.UTF8.GetString(firstResponseBuffer));
@@ -41,14 +43,14 @@ public static class Client
         var secondFrame = "This is the second segment"u8.ToArray();
         var secondResponseBuffer = new byte[secondFrame.Length];
 
-        await client.SendAsync(secondFrame, WebSocketMessageType.Text, true, TimeoutToken());
-        response = await client.ReceiveAsync(new ArraySegment<byte>(secondResponseBuffer), TimeoutToken());
+        await client.SendAsync(secondFrame, WebSocketMessageType.Text, true, token);
+        response = await client.ReceiveAsync(new ArraySegment<byte>(secondResponseBuffer), token);
 
         Assert.AreEqual(WebSocketMessageType.Text, response.MessageType);
         Assert.AreEqual("This is the second segment", Encoding.UTF8.GetString(secondResponseBuffer));
 
         // Close connection
-        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "bye", TimeoutToken());
+        await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "bye", token);
 
         try
         {
@@ -61,6 +63,6 @@ public static class Client
         }
     }
 
-    private static CancellationToken TimeoutToken(int ms = 2000) => new CancellationTokenSource(ms).Token;
+    //private static CancellationToken TimeoutToken(int ms = 2000) => new CancellationTokenSource(ms).Token;
 
 }
