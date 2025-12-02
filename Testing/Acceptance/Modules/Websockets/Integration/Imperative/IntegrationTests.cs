31ba12c0d924f24c6ec9b5a2892d0ef6d657dac3
Andreas Nägeli <andr.naegeli@gmail.com>
2025-12-02 14:32:54 +0100

    Remove the log statements

--- Testing/Acceptance/Modules/Websockets/Integration/Imperative/IntegrationTests.cs
+++ Testing/Acceptance/Modules/Websockets/Integration/Imperative/IntegrationTests.cs
@@ -30,22 +30,14 @@ public sealed class IntegrationTests
         {
             try
             {
-                Console.WriteLine("1: HandleAsync entered");
-
                 var arrayPool = ArrayPool<byte>.Shared;
                 var buffer = arrayPool.Rent(8192);
 
-                Console.WriteLine("2: BeforePing");
-
                 await connection.PingAsync();
-
-                Console.WriteLine("3: AfterPing");
 
                 while (connection.Request.Server.Running)
                 {
-                    Console.WriteLine("4: BeforeRead");
                     var frame = await connection.ReadAsync(buffer);
-                    Console.WriteLine("5: AfterRead");
 
                     if (frame.Type == FrameType.Error)
                     {
@@ -56,24 +48,17 @@ public sealed class IntegrationTests
 
                     if (frame.Type == FrameType.Pong)
                     {
-                        Console.WriteLine("6: ReceivedPong");
                         continue;
                     }
 
                     if (frame.Type == FrameType.Close)
                     {
-                        Console.WriteLine("7: ReceivedClose");
                         await connection.CloseAsync();
-                        Console.WriteLine("8: AfterClose");
                         break;
                     }
 
-                    Console.WriteLine("9: BeforeWrite");
                     await connection.WriteAsync(frame.Data, FrameType.Text, fin: true);
-                    Console.WriteLine("10: AfterWrite");
                 }
-
-                Console.WriteLine("11: End");
 
                 // End
                 arrayPool.Return(buffer);
