e48747cce9792cb9faa8e844587ec96a5434b2fd
Diogo Martins <diogoalves@ua.pt>
2025-12-03 18:19:00 +0000

    Add all integration tests, including start pings

--- Testing/Acceptance/Modules/Websockets/Integration/Imperative/IntegrationTests.cs
+++ Testing/Acceptance/Modules/Websockets/Integration/Imperative/IntegrationTests.cs
@@ -10,7 +10,7 @@ public sealed class IntegrationTests
 {
 
     [TestMethod]
-    public async Task TestServer()
+    public async Task TestServerImperative()
     {
         var websocket = GenHTTP.Modules.Websockets.Websocket
             .CreateImperative()
@@ -21,6 +21,18 @@ public sealed class IntegrationTests
         await using var host = await TestHost.RunAsync(websocket);
 
         await Client.Execute(host.Port);
+    }
+    
+    [TestMethod]
+    public async Task TestServerImperativeFragmented()
+    {
+        var websocket = GenHTTP.Modules.Websockets.Websocket
+            .CreateImperative()
+            .Handler(new MyHandlerFragmented());
+
+        Chain.Works(websocket);
+
+        await using var host = await TestHost.RunAsync(websocket);
 
         await Client.ExecuteFragmented("127.0.0.1", host.Port);
     }
@@ -32,8 +44,48 @@ public sealed class IntegrationTests
         {
             try
             {
-                // await connection.PingAsync();
+                await connection.PingAsync();
             
+                while (connection.Request.Server.Running)
+                {
+                    var frame = await connection.ReadFrameAsync();
+
+                    if (frame.Type == FrameType.Error)
+                    {
+                        var error = frame.FrameError!;
+                        Console.WriteLine($"{error.ErrorType}: {error.Message}");
+                        continue;
+                    }
+
+                    if (frame.Type == FrameType.Pong)
+                    {
+                        continue;
+                    }
+
+                    if (frame.Type == FrameType.Close)
+                    {
+                        await connection.CloseAsync();
+                        break;
+                    }
+
+                    await connection.WriteAsync(frame.Data, FrameType.Text, fin: true);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+    
+    public class MyHandlerFragmented : IImperativeHandler
+    {
+
+        public async ValueTask HandleAsync(IImperativeConnection connection)
+        {
+            try
+            {
                 while (connection.Request.Server.Running)
                 {
                     var frame = await connection.ReadFrameAsync();
--- Testing/Acceptance/Modules/Websockets/Integration/Reactive/IntegrationTests.cs
+++ Testing/Acceptance/Modules/Websockets/Integration/Reactive/IntegrationTests.cs
@@ -9,7 +9,7 @@ public sealed class IntegrationTests
 {
 
     [TestMethod]
-    public async Task TestServer()
+    public async Task TestServerReactive()
     {
         // todo: create another flavor "Functional" out of this
 
@@ -34,11 +34,44 @@ public sealed class IntegrationTests
         await using var host = await TestHost.RunAsync(websocket);
 
         await Client.Execute(host.Port);
+    }
+    
+    [TestMethod]
+    public async Task TestServerReactiveFragmented()
+    {
+        var websocket = GenHTTP.Modules.Websockets.Websocket.CreateReactive()
+            .MaxFrameSize(1024)
+            .Handler(new ReactiveHandlerFragmented());
+
+        Chain.Works(websocket);
+
+        await using var host = await TestHost.RunAsync(websocket);
 
         await Client.ExecuteFragmented("127.0.0.1", host.Port);
     }
 
     public class ReactiveHandler : IReactiveHandler
+    {
+
+        public async ValueTask OnConnected(IReactiveConnection connection) => await connection.PingAsync();
+
+        public async ValueTask OnMessage(IReactiveConnection connection, WebsocketFrame message) => await connection.WriteAsync(message.Data);
+
+        public async ValueTask OnContinue(IReactiveConnection connection, WebsocketFrame message) => await connection.WriteAsync(message.Data);
+
+        public async ValueTask OnPing(IReactiveConnection connection, WebsocketFrame message) => await connection.PongAsync(message.Data);
+
+        public async ValueTask OnClose(IReactiveConnection connection, WebsocketFrame message) => await connection.CloseAsync();
+
+        public ValueTask<bool> OnError(IReactiveConnection connection, FrameError error)
+        {
+            Console.WriteLine($"{error.ErrorType}: {error.Message}");
+            return ValueTask.FromResult(false);
+        }
+
+    }
+    
+    public class ReactiveHandlerFragmented : IReactiveHandler
     {
 
         public ValueTask OnConnected(IReactiveConnection connection) => ValueTask.CompletedTask;
