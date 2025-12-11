d948b9411d2b1aa5d145f0b9e05eeecde293ec48
Diogo Martins <diogoalves@ua.pt>
2025-12-11 22:50:07 +0000

    revert Playground

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,64 +1,10 @@
-﻿using System.Net.WebSockets;
-using System.Text;
-
-using GenHTTP.Engine.Internal;
+﻿using GenHTTP.Engine.Internal;
+using GenHTTP.Modules.IO;
 using GenHTTP.Modules.Practices;
 
-using GenHTTP.Modules.ReverseProxy;
-using GenHTTP.Modules.Websockets;
-using GenHTTP.Modules.Websockets.Protocol;
+var content = Content.From(Resource.FromString("Hello World!"));
 
-// Server
-//
-var proxyHost = Host.Create()
-          .Port(8080)
-          .Handler(Proxy
-              .Create()
-              //.Upstream("wss://ws.postman-echo.com/raw"))
-              .Upstream("ws://localhost:5000"))
-          .Defaults()
-          .StartAsync(); // or StartAsync() for non-blocking
-
-var upstreamServer = Host.Create()
-    .Port(5000)
-    .Handler(Websocket.Functional()
-                .OnMessage((connection, message) =>
-                {
-                    connection.WriteAsync(
-                        message.Data, 
-                        FrameType.Text, 
-                        true, 
-                        CancellationToken.None);
-                    
-                    return ValueTask.CompletedTask;
-                })
-        ).StartAsync();
-
-//
-// Downstream
-await Task.Run(async () =>
-{
-    var client = new ClientWebSocket();
-
-    await client.ConnectAsync(new Uri($"ws://localhost:{8080}"), CancellationToken.None);
-
-    while (true)
-    {
-        var message = "Hello from the other side?"u8.ToArray();
-
-        Console.WriteLine($"[Downstream] - Sending: {Encoding.UTF8.GetString(message)}");
-        
-        await client.SendAsync(
-            message, 
-            WebSocketMessageType.Text, 
-            true, 
-            CancellationToken.None);
-        
-        var responseBuffer = new byte[message.Length];
-        await client.ReceiveAsync(new ArraySegment<byte>(responseBuffer), CancellationToken.None);
-
-        Console.WriteLine($"[Downstream] - Received: {Encoding.UTF8.GetString(responseBuffer, 0, responseBuffer.Length)}");
-        
-        await Task.Delay(500);
-    }
-});
+await Host.Create()
+    .Handler(content)
+    .Defaults()
+    .RunAsync(); // or StartAsync() for non-blocking
