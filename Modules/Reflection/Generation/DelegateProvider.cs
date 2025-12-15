dacec5312fad4d3256d5cc28356992aac5a866dc
Andreas Nägeli <andr.naegeli@gmail.com>
2025-12-15 08:47:04 +0100

    Switch to release mode in codegen

--- Modules/Reflection/Generation/DelegateProvider.cs
+++ Modules/Reflection/Generation/DelegateProvider.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 using GenHTTP.Api.Protocol;
 
@@ -15,41 +16,46 @@ public static class DelegateProvider
         var syntaxTree = CSharpSyntaxTree.ParseText(code);
 
         var assemblyName = Path.GetRandomFileName();
-        
+
         var references = AppDomain.CurrentDomain.GetAssemblies()
                                   .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
                                   .Select(a => MetadataReference.CreateFromFile(a.Location))
                                   .Cast<MetadataReference>();
 
-        var compilation = CSharpCompilation.Create(
-            assemblyName,
-            new[] { syntaxTree },
-            references,
-            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+        var compilation = CSharpCompilation.Create(assemblyName, [syntaxTree], references,
+                                                   new CSharpCompilationOptions(
+                                                       OutputKind.DynamicallyLinkedLibrary,
+                                                       optimizationLevel: OptimizationLevel.Release,
+                                                       concurrentBuild: true,
+                                                       deterministic: false
+                                                   )
         );
 
         using var ms = new MemoryStream();
-        
+
         var result = compilation.Emit(ms);
 
         if (!result.Success)
         {
             var errors = string.Join(Environment.NewLine, result.Diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error));
-            
+
             throw new InvalidOperationException("Failed to compile handler:" + Environment.NewLine + Environment.NewLine + errors);
         }
 
         ms.Seek(0, SeekOrigin.Begin);
-        
+
         var assembly = Assembly.Load(ms.ToArray());
-        
+
         var type = assembly.GetType("Invoker");
-        
+
         var method = type!.GetMethod("Invoke")!;
+
+        // warm up the JIT
+        RuntimeHelpers.PrepareMethod(method.MethodHandle);
 
         return (Func<T, IRequest, MethodRegistry, ValueTask<IResponse?>>)Delegate.CreateDelegate(
             typeof(Func<T, IRequest, MethodRegistry, ValueTask<IResponse?>>), method
         );
     }
-    
+
 }
