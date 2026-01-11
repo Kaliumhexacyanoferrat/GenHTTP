e0ce0dbbd0518d19058a6cbb490e6cf9169b0e4e
Andreas Nägeli <andr.naegeli@gmail.com>
2026-01-11 16:28:25 +0100

    Add integration tests

---
+++ Testing/Acceptance/Modules/DependencyInjection/WebsocketTests.Imperative.cs
@@ -0,0 +1,37 @@
+﻿using GenHTTP.Modules.DependencyInjection;
+using GenHTTP.Testing.Acceptance.Modules.Websockets.Integration;
+
+using WS = GenHTTP.Modules.Websockets;
+using IWS = GenHTTP.Testing.Acceptance.Modules.Websockets.Integration.Imperative;
+
+namespace GenHTTP.Testing.Acceptance.Modules.DependencyInjection;
+
+
+[TestClass]
+public class ImperativeWebsocketTests
+{
+
+    [TestMethod]
+    public async Task TestReactiveHandler()
+    {
+        var websocket = WS.Websocket.Imperative()
+                          .DependentHandler<Handler>();
+
+        await using var host = await DependentHost.RunAsync(websocket);
+
+        await Client.Execute(host.Port);
+    }
+
+    private class Handler(AwesomeService dependency) : WS.IImperativeHandler
+    {
+
+        public ValueTask HandleAsync(WS.IImperativeConnection connection)
+        {
+            Assert.AreEqual("42", dependency.DoWork());
+
+            return new IWS.IntegrationTests.MyHandler().HandleAsync(connection);
+        }
+
+    }
+
+}
---
+++ Testing/Acceptance/Modules/DependencyInjection/WebsocketTests.Reactive.cs
@@ -0,0 +1,37 @@
+﻿using GenHTTP.Modules.DependencyInjection;
+using GenHTTP.Testing.Acceptance.Modules.Websockets.Integration;
+
+using WS = GenHTTP.Modules.Websockets;
+
+namespace GenHTTP.Testing.Acceptance.Modules.DependencyInjection;
+
+[TestClass]
+public class ReactiveWebsocketTests
+{
+
+    [TestMethod]
+    public async Task TestReactiveHandler()
+    {
+        var websocket = WS.Websocket.Reactive()
+                          .DependentHandler<Handler>();
+
+        await using var host = await DependentHost.RunAsync(websocket);
+
+        await Client.Execute(host.Port);
+    }
+
+    private class Handler(AwesomeService dependency) : WS.IReactiveHandler
+    {
+
+        public async ValueTask OnConnected(WS.IReactiveConnection connection)
+        {
+            Assert.AreEqual("42", dependency.DoWork());
+
+            await connection.PingAsync();
+        }
+
+        public async ValueTask OnMessage(WS.IReactiveConnection connection, WS.IWebsocketFrame message) => await connection.WriteAsync(message.Data);
+
+    }
+
+}
