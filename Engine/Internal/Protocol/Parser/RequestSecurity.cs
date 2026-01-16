ff47509cd496dff31ca1706cfa18c1a901a8c677
Andreas Nägeli <andr.naegeli@gmail.com>
2026-01-16 17:21:51 +0100

    Harden against multiple host headers

---
+++ Engine/Internal/Protocol/Parser/RequestSecurity.cs
@@ -0,0 +1,21 @@
+﻿namespace GenHTTP.Engine.Internal.Protocol.Parser;
+
+internal static class RequestSecurity
+{
+
+    public static void Validate(Request request)
+    {
+        var headers = request.Headers;
+
+        if (!headers.TryGetValue("Host", out var host))
+        {
+            throw new ProtocolException("Mandatory 'Host' header is missing from the request");
+        }
+
+        if (request.ContainsMultipleHeaders("Host"))
+        {
+            throw new ProtocolException("Multiple 'Host' headers specified");
+        }
+    }
+
+}
