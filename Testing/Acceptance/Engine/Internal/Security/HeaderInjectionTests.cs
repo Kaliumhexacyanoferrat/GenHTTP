1041342028f5b1f5ab2aff6ce358b29c3c16b291
Andreas Nägeli <andr.naegeli@gmail.com>
2026-01-20 12:26:37 +0100

    Ensure request headers cannot be injected (#809)

---
+++ Testing/Acceptance/Engine/Internal/Security/HeaderInjectionTests.cs
@@ -0,0 +1,42 @@
+﻿using GenHTTP.Api.Protocol;
+using GenHTTP.Modules.Functional;
+
+namespace GenHTTP.Testing.Acceptance.Engine.Internal.Security;
+
+[TestClass]
+public class HeaderInjectionTests : WireTest
+{
+
+    [TestMethod]
+    public async Task TryViaBackslash()
+    {
+        var request = new []
+        {
+            "GET / HTTP/1.1",
+            "Host: bla\\r\\nInjected:Yes"
+        };
+
+        await TestInjection(request);
+    }
+
+    [TestMethod]
+    public async Task TryViaEncoding()
+    {
+        var request = new []
+        {
+            "GET / HTTP/1.1",
+            "Host: bla%0D%0AInjected:Yes"
+        };
+
+        await TestInjection(request);
+    }
+
+    private async Task TestInjection(string[] request)
+    {
+        var app = Inline.Create()
+                        .Get((IRequest r) => r.Headers.ContainsKey("Injected") ? "Yes" : "No");
+
+        await TestAsync(request, "No");
+    }
+
+}
