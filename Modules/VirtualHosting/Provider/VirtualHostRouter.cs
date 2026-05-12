a58956f14e2c657b6855a408aab71d7d952658c6
Andreas Nägeli <andr.naegeli@gmail.com>
2026-05-12 14:25:47 +0200

    Re-enable virtual hosting module

--- Modules/VirtualHosting/Provider/VirtualHostRouter.cs
+++ Modules/VirtualHosting/Provider/VirtualHostRouter.cs
@@ -1,16 +1,18 @@
-﻿using GenHTTP.Api.Content;
-using GenHTTP.Api.Protocol;
+﻿using System.Collections.Frozen;
 
-using GenHTTP.Modules.IO;
+using GenHTTP.Api.Content;
+using GenHTTP.Api.Protocol;
+using GenHTTP.Api.Protocol.Raw;
 
 namespace GenHTTP.Modules.VirtualHosting.Provider;
 
 public sealed class VirtualHostRouter : IHandler
 {
+    private static readonly ReadOnlyMemory<byte> HostHeader = "Host"u8.ToArray();
 
     #region Get-/Setters
 
-    private Dictionary<string, IHandler> Hosts { get; }
+    private FrozenDictionary<PathSegment, IHandler> Hosts { get; }
 
     private IHandler? DefaultRoute { get; }
 
@@ -18,9 +20,9 @@ public sealed class VirtualHostRouter : IHandler
 
     #region Initialization
 
-    public VirtualHostRouter(Dictionary<string, IHandlerBuilder> hosts, IHandlerBuilder? defaultRoute)
+    public VirtualHostRouter(Dictionary<PathSegment, IHandlerBuilder> hosts, IHandlerBuilder? defaultRoute)
     {
-        Hosts = hosts.ToDictionary(kv => kv.Key, kv => kv.Value.Build());
+        Hosts = hosts.ToFrozenDictionary(kv => kv.Key, kv => kv.Value.Build());
         DefaultRoute = defaultRoute?.Build();
     }
 
@@ -45,12 +47,12 @@ public sealed class VirtualHostRouter : IHandler
 
     private IHandler? GetHandler(IRequest request)
     {
-        var host = request.HostWithoutPort();
+        var host = GetHostWithoutPort(request);
 
         // try to find a regular route
         if (host is not null)
         {
-            if (Hosts.TryGetValue(host, out var handler))
+            if (Hosts.TryGetValue(new(host.Value), out var handler))
             {
                 return handler;
             }
@@ -58,6 +60,21 @@ public sealed class VirtualHostRouter : IHandler
 
         // route by default
         return DefaultRoute;
+    }
+
+    private static ReadOnlyMemory<byte>? GetHostWithoutPort(IRequest request)
+    {
+        var host = request.Raw.Header.Headers.GetEntry(HostHeader);
+
+        if (host == null)
+        {
+            return null;
+        }
+
+        var hostSpan = host.Value.Span;
+        var colonIndex = hostSpan.IndexOf((byte)':');
+
+        return colonIndex >= 0 ? host.Value[..colonIndex] : host.Value;
     }
 
     #endregion
