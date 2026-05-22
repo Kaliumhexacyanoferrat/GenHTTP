dcd7734fd1197909d46cf0cdce84a0fc2d443b34
Andreas Nägeli <andr.naegeli@gmail.com>
2026-05-22 17:37:16 +0200

    More async

--- Engine/Shared/Types/Body/LengthLimitedStream.cs
+++ Engine/Shared/Types/Body/LengthLimitedStream.cs
@@ -30,6 +30,9 @@ public class LengthLimitedStream : Stream, IDrainableStream
         _contentLength = contentLength;
         _bytesRemaining = contentLength;
     }
+
+    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        => ReadAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
     
     public override async ValueTask<int> ReadAsync(Memory<byte> destination, CancellationToken cancellationToken = default)
     {
