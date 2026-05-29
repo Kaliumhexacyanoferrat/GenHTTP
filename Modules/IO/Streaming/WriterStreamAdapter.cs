a137f7e6b5180f42ec555827d75a99e8c8a23223
Andreas Nägeli <andr.naegeli@gmail.com>
2026-05-29 10:10:27 +0200

    Code formatting etc.

--- Modules/IO/Streaming/WriterStreamAdapter.cs
+++ Modules/IO/Streaming/WriterStreamAdapter.cs
@@ -6,14 +6,27 @@ public sealed class WriterStreamAdapter : Stream
 {
     private readonly IBufferWriter<byte> _writer;
 
-    public WriterStreamAdapter(IBufferWriter<byte> writer) => _writer = writer;
-
+    #region Get-/Setters
+    
     public override bool CanRead => false;
+    
     public override bool CanSeek => false;
+    
     public override bool CanWrite => true;
 
     public override long Length => throw new NotSupportedException();
+    
     public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+    
+    #endregion
+    
+    #region Initialization
+    
+    public WriterStreamAdapter(IBufferWriter<byte> writer) => _writer = writer;
+
+    #endregion
+    
+    #region Functionality
 
     public override void Write(ReadOnlySpan<byte> buffer)
     {
@@ -27,10 +40,17 @@ public sealed class WriterStreamAdapter : Stream
         }
     }
 
-    public override void Write(byte[] buffer, int offset, int count) => Write(buffer.AsSpan(offset, count));
+    public override void Write(byte[] buffer, int offset, int count) 
+        => Write(buffer.AsSpan(offset, count));
 
     public override void Flush() { }
+    
     public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+    
     public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+    
     public override void SetLength(long value) => throw new NotSupportedException();
+    
+    #endregion
+    
 }
