4eec55f59db849ca25f182dd581b0f77cc1f9a1d
Andreas Nägeli <andr.naegeli@gmail.com>
2026-06-03 13:46:41 +0200

    Add formatter tests

---
+++ Testing/Acceptance/Modules/Conversion/Formatters/PrimitiveFormatterTests.cs
@@ -0,0 +1,89 @@
+using System.Text;
+
+using GenHTTP.Modules.Conversion.Formatters;
+
+namespace GenHTTP.Testing.Acceptance.Modules.Conversion.Formatters;
+
+[TestClass]
+public sealed class PrimitiveFormatterTests
+{
+    private readonly PrimitiveFormatter _formatter = new();
+
+    private static ReadOnlyMemory<byte> Bytes(string s) => Encoding.UTF8.GetBytes(s);
+
+    [TestMethod]
+    public void HandlesPrimitiveTypes()
+    {
+        Assert.IsTrue(_formatter.CanHandle(typeof(int)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(long)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(short)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(byte)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(double)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(float)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(bool)));
+        Assert.IsTrue(_formatter.CanHandle(typeof(char)));
+    }
+
+    [TestMethod]
+    public void DoesNotHandleNonPrimitives()
+    {
+        Assert.IsFalse(_formatter.CanHandle(typeof(string)));
+        Assert.IsFalse(_formatter.CanHandle(typeof(Guid)));
+        Assert.IsFalse(_formatter.CanHandle(typeof(decimal)));
+    }
+
+    [TestMethod]
+    public void ReadsInt() => Assert.AreEqual(42, _formatter.Read(Bytes("42"), typeof(int)));
+
+    [TestMethod]
+    public void ReadsNegativeInt() => Assert.AreEqual(-100, _formatter.Read(Bytes("-100"), typeof(int)));
+
+    [TestMethod]
+    public void ThrowsForInvalidInt() =>
+        Assert.ThrowsExactly<ArgumentException>(() => _formatter.Read(Bytes("abc"), typeof(int)));
+
+    [TestMethod]
+    public void ReadsLong() => Assert.AreEqual(1_000_000_000_000L, _formatter.Read(Bytes("1000000000000"), typeof(long)));
+
+    [TestMethod]
+    public void ReadsShort() => Assert.AreEqual((short)32767, _formatter.Read(Bytes("32767"), typeof(short)));
+
+    [TestMethod]
+    public void ReadsByte() => Assert.AreEqual((byte)255, _formatter.Read(Bytes("255"), typeof(byte)));
+
+    [TestMethod]
+    public void ReadsDouble() => Assert.AreEqual(3.14, (double)_formatter.Read(Bytes("3.14"), typeof(double))!, 1e-10);
+
+    [TestMethod]
+    public void ReadsFloat() => Assert.AreEqual(1.5f, (float)_formatter.Read(Bytes("1.5"), typeof(float))!, 1e-6f);
+
+    [TestMethod]
+    public void ReadsBoolCaseInsensitive()
+    {
+        Assert.AreEqual(true, _formatter.Read(Bytes("true"), typeof(bool)));
+        Assert.AreEqual(true, _formatter.Read(Bytes("True"), typeof(bool)));
+        Assert.AreEqual(true, _formatter.Read(Bytes("TRUE"), typeof(bool)));
+        Assert.AreEqual(false, _formatter.Read(Bytes("false"), typeof(bool)));
+        Assert.AreEqual(false, _formatter.Read(Bytes("False"), typeof(bool)));
+    }
+
+    [TestMethod]
+    public void ThrowsForInvalidBool() =>
+        Assert.ThrowsExactly<ArgumentException>(() => _formatter.Read(Bytes("yes"), typeof(bool)));
+
+    [TestMethod]
+    public void ReadsChar() => Assert.AreEqual('A', _formatter.Read(Bytes("A"), typeof(char)));
+
+    [TestMethod]
+    public void ThrowsForMultiCharInput() =>
+        Assert.ThrowsExactly<ArgumentException>(() => _formatter.Read(Bytes("AB"), typeof(char)));
+
+    [TestMethod]
+    public void WritesInt() => Assert.AreEqual("42", _formatter.Write(42, typeof(int)));
+
+    [TestMethod]
+    public void WritesDoubleInInvariantCulture() => Assert.AreEqual("3.14", _formatter.Write(3.14, typeof(double)));
+
+    [TestMethod]
+    public void WritesBoolAsWord() => Assert.AreEqual("True", _formatter.Write(true, typeof(bool)));
+}
---
+++ Testing/Acceptance/Modules/Conversion/Formatters/StringFormatterTests.cs
@@ -0,0 +1,29 @@
+using System.Text;
+
+using GenHTTP.Modules.Conversion.Formatters;
+
+namespace GenHTTP.Testing.Acceptance.Modules.Conversion.Formatters;
+
+[TestClass]
+public sealed class StringFormatterTests
+{
+    private readonly StringFormatter _formatter = new();
+
+    private static ReadOnlyMemory<byte> Bytes(string s) => Encoding.UTF8.GetBytes(s);
+
+    [TestMethod] public void HandlesOnlyString()
+    {
+        Assert.IsTrue(_formatter.CanHandle(typeof(string)));
+        Assert.IsFalse(_formatter.CanHandle(typeof(int)));
+        Assert.IsFalse(_formatter.CanHandle(typeof(bool)));
+    }
+
+    [TestMethod] public void ReadsAsciiString() => Assert.AreEqual("hello", _formatter.Read(Bytes("hello"), typeof(string)));
+
+    [TestMethod] public void ReadsEmptyString() => Assert.AreEqual("", _formatter.Read(Bytes(""), typeof(string)));
+
+    [TestMethod] public void ReadsUtf8String() => Assert.AreEqual("héllo", _formatter.Read(Bytes("héllo"), typeof(string)));
+
+    [TestMethod] public void WritesString() => Assert.AreEqual("hello", _formatter.Write("hello", typeof(string)));
+    
+}
