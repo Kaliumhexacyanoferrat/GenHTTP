3888d02021f18c219de9722376c328ae3606ffb8
Andreas Nägeli <andr.naegeli@gmail.com>
2026-07-10 16:17:48 +0200

    Properly map chunked parsing errors

--- Testing/Acceptance/Engine/Compliance/ComplianceTests.cs
+++ Testing/Acceptance/Engine/Compliance/ComplianceTests.cs
@@ -1,4 +1,5 @@
-﻿using GenHTTP.Modules.Layouting;
+﻿using GenHTTP.Modules.Functional;
+using GenHTTP.Modules.Layouting;
 
 namespace GenHTTP.Testing.Acceptance.Engine.Compliance;
 
@@ -10,8 +11,6 @@ public class ComplianceTests : WireTest
     [MultiEngineTest]
     public async Task TestNoMinorHttpVersion(TestEngine engine)
     {
-        await using var host = await TestHost.RunAsync(Layout.Create(), engine: engine);
-
         await TestAsync(["GET / HTTP/1", "Host: localhost"], "505", Layout.Create(), engine);
     }
 
@@ -19,8 +18,6 @@ public class ComplianceTests : WireTest
     [MultiEngineTest]
     public async Task TestNonSupportedVersion(TestEngine engine)
     {
-        await using var host = await TestHost.RunAsync(Layout.Create(), engine: engine);
-
         await TestAsync(["GET / HTTP/1.2", "Host: localhost"], "505", Layout.Create(), engine);
     }
 
@@ -28,8 +25,6 @@ public class ComplianceTests : WireTest
     [MultiEngineTest]
     public async Task TestEmptyHost(TestEngine engine)
     {
-        await using var host = await TestHost.RunAsync(Layout.Create(), engine: engine);
-
         await TestAsync(["GET / HTTP/1.0", "Host: "], "400", Layout.Create(), engine);
     }
 
@@ -37,8 +32,6 @@ public class ComplianceTests : WireTest
     [MultiEngineTest]
     public async Task TestPostNoBody(TestEngine engine)
     {
-        await using var host = await TestHost.RunAsync(Layout.Create(), engine: engine);
-
         await TestAsync(["POST / HTTP/1.0", "Host: host"], "404", Layout.Create(), engine);
     }
 
@@ -46,9 +39,25 @@ public class ComplianceTests : WireTest
     [MultiEngineTest]
     public async Task TestGetWithBody(TestEngine engine)
     {
-        await using var host = await TestHost.RunAsync(Layout.Create(), engine: engine);
-
         await TestAsync(["GET / HTTP/1.0", "Host: host", "Content-Length: 3", "", "123"], "400", Layout.Create(), engine);
+    }
+
+    [TestMethod]
+    [MultiEngineTest]
+    public async Task TestChunkWithSemicolon(TestEngine engine)
+    {
+        var handler = Inline.Create().Post(async (Stream body) =>
+        {
+            var memoryStream = new MemoryStream();
+    
+            await body.CopyToAsync(memoryStream);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream;
+        });
+
+        await TestAsync(["POST / HTTP/1.0", "Host: host", "Transfer-Encoding: chunked", "", "5;", "hello", "", "0", ""], "400", handler, engine);
     }
 
 }
